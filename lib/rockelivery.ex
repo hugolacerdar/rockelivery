f9fc9ee72d2846646fc2b84211a1be8568a6c34d
Hugo <hugolacerdar@gmail.com>
2021-05-20 15:53:36 -0300

    Adding delete user facade

--- lib/rockelivery.ex
+++ lib/rockelivery.ex
@@ -1,7 +1,9 @@
 defmodule Rockelivery do
   alias Rockelivery.Users.Create, as: UserCreate
+  alias Rockelivery.Users.Delete, as: UserDelete
   alias Rockelivery.Users.Get, as: UserGet
 
   defdelegate create_user(params), to: UserCreate, as: :call
   defdelegate get_user_by_id(params), to: UserGet, as: :by_id
+  defdelegate delete_user(params), to: UserDelete, as: :call
 end
