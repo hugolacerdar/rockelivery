e824189908ea0116ae949b3d5c454322396a65f7
Hugo <hugolacerdar@gmail.com>
2021-05-24 20:36:03 -0300

    Adding item struct

---
+++ lib/rockelivery/item.ex
@@ -0,0 +1,34 @@
+defmodule Rockelivery.Item do
+  use Ecto.Schema
+  import Ecto.Changeset
+  alias Ecto.Enum
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+
+  @required_params [:category, :description, :price, :photo]
+  @item_category [:food, :drink, :dessert]
+
+  @derive {Jason.Encoder, only: @required_params ++ [:id]}
+
+  schema "items" do
+    field :category, Enum, values: @item_category
+    field :description, :string
+    field :price, :decimal
+    field :photo, :string
+
+    timestamps()
+  end
+
+  def changeset(params) do
+    %__MODULE__{}
+    |> cast(params, @required_params)
+    |> validate_required(@required_params)
+    |> validate_length(:description, min: 5)
+    |> validate_number(:price, greater_than: 0)
+  end
+
+  def changeset(struct, params) do
+    struct
+    |> cast(params, @required_params)
+  end
+end
