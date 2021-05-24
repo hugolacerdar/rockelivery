2100fe40b80e63e95dd33861f752bb7cb9e0c5d6
Hugo <hugolacerdar@gmail.com>
2021-05-23 22:23:49 -0300

    Adding user creation test

---
+++ test/rockelivery/users/create_test.exs
@@ -0,0 +1,32 @@
+defmodule Rockelivery.Users.CreateTest do
+  use Rockelivery.DataCase, async: true
+
+  import Rockelivery.Factory
+
+  alias Rockelivery.{Error, User}
+  alias Rockelivery.Users.Create
+
+  describe "call/1" do
+    test "when all params are valid, returns the user" do
+      params = build(:user_params)
+
+      response = Create.call(params)
+
+      assert {:ok, %User{id: _id, age: 26, email: "hugo@mailc.com"}} = response
+    end
+
+    test "when any of the params are invalid, returns an error" do
+      params = build(:user_params, %{"age" => 11, "cpf" => "123456"})
+
+      response = Create.call(params)
+
+      expected = %{
+        age: ["must be greater than or equal to 18"],
+        cpf: ["should be 11 character(s)"]
+      }
+
+      assert {:error, %Error{status: :bad_request, result: changeset}} = response
+      assert errors_on(changeset) == expected
+    end
+  end
+end
