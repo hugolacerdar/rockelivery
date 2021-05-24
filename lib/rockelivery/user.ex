9960c4b1875a05b12a191f0d521a6bdc05173aec
Hugo <hugolacerdar@gmail.com>
2021-05-23 22:19:26 -0300

    Fixing password check

--- lib/rockelivery/user.ex
+++ lib/rockelivery/user.ex
@@ -25,7 +25,7 @@ defmodule Rockelivery.User do
     %__MODULE__{}
     |> cast(params, @required_params)
     |> validate_required(@required_params)
-    |> validate_length(:password_hash, min: 6)
+    |> validate_length(:password, min: 6)
     |> validate_length(:cep, is: 8)
     |> validate_length(:cpf, is: 11)
     |> validate_number(:age, greater_than_or_equal_to: 18)
@@ -38,7 +38,7 @@ defmodule Rockelivery.User do
   def changeset(struct, params) do
     struct
     |> cast(params, @required_params)
-    |> validate_length(:password_hash, min: 6)
+    |> validate_length(:password, min: 6)
     |> validate_length(:cep, is: 8)
     |> validate_length(:cpf, is: 11)
     |> validate_number(:age, greater_than_or_equal_to: 18)
