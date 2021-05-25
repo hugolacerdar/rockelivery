3823396331a10a65f5df5fa1d633fe5b5f2f1343
Hugo <hugolacerdar@gmail.com>
2021-05-25 20:07:20 -0300

    Adding order params factory

--- test/support/factory.ex
+++ test/support/factory.ex
@@ -27,4 +27,17 @@ defmodule Rockelivery.Factory do
       id: "2a723b3f-8850-452c-bdbf-28b81edefd32"
     }
   end
+
+  def order_params_factory do
+    %{
+      "user_id" => "646ecc16-3499-4cc3-96d7-920ae61ca680",
+      "items" => [
+        %{"id" => "3a36f4db-aa0b-44bd-ac2d-19511bb3b423", "quantity" => 2},
+        %{"id" => "d5baec7b-8610-4cd2-a843-ffa5cd5342c6", "quantity" => 4}
+      ],
+      "address" => "Rua 2 - Bairro Três",
+      "comments" => "Sem banana",
+      "payment_method" => "money"
+    }
+  end
 end
