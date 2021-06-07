0c7389a4d2aaab70f877d80dbc146e646944b65d
Hugo <hugolacerdar@gmail.com>
2021-06-07 14:52:16 -0300

    Adding seeds file

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,46 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias Rockelivery.{Item, Order, Repo, User}
+
+user_params = %{
+  age: 27,
+  address: "Rua Ca",
+  cep: "12345678",
+  cpf: "12345678901",
+  email: "banana@banana.com",
+  password: 123_456,
+  name: "Hugo"
+}
+
+user_changeset = User.changeset(user_params)
+
+%User{id: user_id} = Repo.insert!(user_changeset)
+
+item1 = %Item{
+  category: :food,
+  description: "Banana Frita",
+  price: Decimal.new("15.50"),
+  photo: "priv/photos/banana_frita.png"
+}
+
+item2 = %Item{
+  category: :food,
+  description: "Banana Assada",
+  price: Decimal.new("10.30"),
+  photo: "priv/photos/banana_assada.png"
+}
+
+Repo.insert!(item1)
+Repo.insert!(item2)
+
+order = %Order{
+  user_id: user_id,
+  items: [item1, item1, item2],
+  address: "Rua Ca",
+  comments: "Sem canela",
+  payment_method: :money
+}
+
+Repo.insert!(order)
